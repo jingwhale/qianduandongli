4dedb77fda529ab0bbc1e63f374b0cae4a1e7ad0
jingwhale <yunlonglijie@163.com>
2015-03-09 15:40:02 +0800

    d

---
+++ uc_server/data/cache/apps.php
@@ -0,0 +1,23 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://localhost/discuz',
+    'authkey' => 'p59dRf2ff0F362U1McufbeX6ycibw0we44Xfd3R88bM3c0FeKfF1B9Y8F4Uen3A1',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '',
+    'allowips' => '',
+  ),
+);
+
+?>
---
+++ uc_server/data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '');
+define('UC_DBNAME', 'discuz');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', 'pre_ucenter_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'utf-8');
+define('UC_FOUNDERPW', '4735909f41adfe0c224ebc938ffb2a05');
+define('UC_FOUNDERSALT', 'r5id8f');
+define('UC_KEY', 'H5udCfffr0j3p2D1JckfFeh6dcTb50Ee64rfq3N8obI3D0CeRf51O948I4yea3e1');
+define('UC_SITEID', 'o5HdafQft0r3u2z1FcNf5ei6jcKb10beZ4qfE3T8lbT3L0deCfJ1P9C8p43ed3z1');
+define('UC_MYKEY', 't5Zdgfxfi0S392P1HcSfFep6Ocfbs0Fef4qfv3R89bv3Y0qe5fL1f9f8K4bex3b1');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
